70b3d8d00bf2b566c3c9d843a329c758a4284cad
gppCalcagno <gpp.calcagno@gmail.com>
2026-01-28 00:27:51 +0100

    add navbar and fix note widht

---
+++ src/content/static_pages/abouteMe.ts
@@ -0,0 +1,6 @@
+export const AboutMe_data = {
+  firstPar1:"Since I truly believe content is king, I tend to value what's written over who wrote it. Still, if you'd like to know a bit about me, you're in the right place!",
+  firstPar2:"I'm a Computer Science Engineer focused on extracting value from data. I'm prone to forgetting things, so I write things down. This web space functions as my public diary, a personal archive of everything I explore in my life.",
+  role: "Computer Science Engineer",
+  skills: ["Astro", "Tailwind"]
+};
