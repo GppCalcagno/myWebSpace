a344ebffca586c1bc4b41420d358878d6d7cb395
gppCalcagno <gpp.calcagno@gmail.com>
2026-02-07 18:10:31 +0100

    add dipendency for cloudfare deploy and CI\CD

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon()]
+  integrations: [icon()],
+  adapter: cloudflare()
 });
